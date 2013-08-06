d2761faa20200dad68307ff9138537750595d542
Alessandro Franceschi <al@lab42.it>
2013-08-06 14:43:34 +0200

    Added configfile define

---
+++ manifests/configfile.pp
@@ -0,0 +1,48 @@
+#
+# = Define: zabbix_agent::configfile
+#
+# The define manages zabbix_agent configfile
+#
+#
+# == Parameters
+#
+# [*ensure*]
+#   String. Default: present
+#   Manages configfile presence. Possible values:
+#   * 'present' - Install package, ensure files are present.
+#   * 'absent' - Stop service and remove package and managed files
+#
+# [*template*]
+#   String. Default: Provided by the module.
+#   Sets the path of a custom template to use as content of configfile
+#   If defined, configfile file has: content => content("$template")
+#   Example: template => 'site/configfile.conf.erb',
+#
+# [*options*]
+#   Hash. Default undef. Needs: 'template'.
+#   An hash of custom options to be used in templates to manage any key pairs of
+#   arbitrary settings.
+#
+define zabbix_agent::configfile (
+  $template ,
+  $ensure   = present,
+  $options  = '' ,
+  $ensure  = present ) {
+
+  include zabbix_agent
+
+  file { "zabbix_agent_configfile_${name}":
+    ensure  => $ensure,
+    path    => "${zabbix_agent::config_dir}/${name}",
+    mode    => $zabbix_agent::config_file_mode,
+    owner   => $zabbix_agent::config_file_owner,
+    group   => $zabbix_agent::config_file_group,
+    require => Package[$zabbix_agent::package],
+    notify  => $zabbix_agent::manage_registry_service_autorestart,
+    content => template($template),
+    replace => $zabbix_agent::manage_file_replace,
+    audit   => $zabbix_agent::manage_audit,
+    noop    => $zabbix_agent::bool_noops,
+  }
+
+}
